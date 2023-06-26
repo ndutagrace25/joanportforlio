fd0fce66b02de967c324999a1c29b7aef9b9fba2
ndutagrace25 <ndutagrace25@gmail.com>
2023-06-26 14:54:01 +0300

    added first section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,39 +5,36 @@ const Intro = () => {
   return (
     <div className="px-5 mx-5 mt-5">
       <div className="d-flex justify-content-center flex-column align-items-center">
-        <div>
-          <div className="d-flex justify-content-start">
-            <span className="primary-text-color yellow-bg p-3 rounded text-align-center fw-bold fs-5">
-              J.A
-            </span>
-          </div>
+        <div className="intro-width">
           <div className="d-flex justify-content-between pt-3">
             <div className="col-md-6 d-flex flex-column justify-content-center">
               <div className="text-uppercase primary-text-color">
                 <h6 className="name-itro">Hello there my name is:</h6>
               </div>
-              <div className="py-3 fw-bold fs-1">Joan Aluka</div>
+              <h1 className="py-3 fw-bold actual-name">Joan Aluka</h1>
               <div>
-                <p className="fs-4">
+                <p className="fs-4 intro-info">
                   I am a{" "}
                   <span className="fw-bold">Front-End Software Engineer</span>{" "}
-                  with <span className="fw-bold">4+ years</span> of experience in designing & building both web
-                  and mobile application
+                  with <span className="fw-bold">4+ years</span> of experience
+                  in designing & building both web and mobile application
                 </p>
               </div>
-              <div>
-                <button className="text-uppercase btn primary-color text-white py-3 fw-bold">
-                  Download Resume
+              <div className="download-section">
+                <button className="text-uppercase btn primary-color text-white py-3 fw-bold download-btn">
+                  <span className="btn-font">
+                    Download Resume <i className="fas fa-download ps-2"></i>
+                  </span>
                 </button>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 mobile-image-hide d-flex justify-content-center">
               <img
                 className="img-fluid rounded"
                 src={developer}
                 alt="developer"
                 // height="100"
-                width="400"
+                width="300"
               />
             </div>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="px-5 pt-4 d-flex justify-content-end nav-icon">
-      <div className="primary-color p-2 rounded" style={{ cursor: "pointer" }}>
-        <i className="fas fa-bars fa-2xl text-white icon-size"></i>
+    <div className="px-5 pt-4 d-flex justify-content-between nav-section">
+      <div className="d-flex justify-content-start">
+        <span className="primary-text-color yellow-bg p-3 rounded text-align-center fw-bold fs-5 nav-icon">
+          J.A
+        </span>
+      </div>
+      <div className=" ">
+        <div
+          className="primary-color p-3 rounded nav-icon"
+          style={{ cursor: "pointer" }}
+        >
+          <i className="fas fa-bars fa-2xl text-white icon-size"></i>
+        </div>
       </div>
     </div>
   );
