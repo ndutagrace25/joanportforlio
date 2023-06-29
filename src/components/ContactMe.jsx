0da0cf76b2c77e7628367c17974f6c8de59cb6f8
ndutagrace25 <ndutagrace25@gmail.com>
2023-06-29 16:59:49 +0300

    added about me

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
-  )
-}
+    <div className="bg-dark p-5">
+      <div className="container rounded bg-white p-5">
+        <h6 className="primary-text-color fw-bold about-header text-uppercase">
+          Like What You See?
+        </h6>
+        <div className="my-3">
+          <h1 className="text-black fw-bold skill-header">
+            Lets Work Together
+          </h1>
+        </div>
+        <div className="d-flex justify-content-between flex-wrap ">
+          <div className="col-md-5 mt-3">
+            <p>
+              I'm currently available for work, so if you'd like to discuss any
+              projects or potential collaborations, you can reach me though any
+              of the available contact information.
+            </p>
+          </div>
+          <div className="col-md-5 mt-3">
+            <a
+              className="d-flex flex-row align-items-center"
+              style={{ cursor: "pointer" }}
+              href="mailto: mmbonojoan@gmail.com"
+            >
+              <i className="fa-regular fa-envelope fa-lg primary-text-color"></i>
+              <div className="ps-3 fw-bold">mmbonojoan@gmail.com</div>
+            </a>
+            <a
+              className="d-flex flex-row align-items-center py-3"
+              style={{ cursor: "pointer" }}
+              href="tel:+254718149079"
+            >
+              <i className="fa-regular fa-message fa-lg primary-text-color"></i>
+              <div className="ps-3 fw-bold">+254 718 149 079</div>
+            </a>
+            <a
+              className="d-flex flex-row align-items-center pb-3"
+              style={{ cursor: "pointer" }}
+              href="https://www.linkedin.com/in/joan-aluka/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-linkedin fa-lg primary-text-color"></i>
+              <div className="ps-3 fw-bold">Linkedin</div>
+            </a>
+            <a
+              className="d-flex flex-row align-items-center"
+              style={{ cursor: "pointer" }}
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/joannealuka"
+            >
+              <i className="fa-brands fa-square-github fa-lg primary-text-color"></i>
+              <div className="ps-3 fw-bold">Github</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
